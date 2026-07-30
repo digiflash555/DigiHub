538b0839b4f22fa43be7b429db1ac57e81d4025b
digiflash555 <digiflash555@gmail.com>
2026-07-30 08:24:51 +0530

    Delete server directory

--- server/viewForms.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI).then(async () => {
-  const forms = await require('./src/models/NominationForm').find().lean();
-  console.log(JSON.stringify(forms, null, 2));
-  process.exit();
-});
