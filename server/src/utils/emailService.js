d2bd0e14f6d82b6bd7dd3884c9fd55c300658eeb
digiflash555 <digiflash555@gmail.com>
2026-08-21 19:12:03 +0530

    Delete server/src directory

--- server/src/utils/emailService.js
+++
@@ -1,8 +0,0 @@
-// Email feature has been removed from this application.
-// This stub is kept to avoid import errors if any old references remain.
-
-const sendEmail = async ({ to, subject, htmlContent }) => {
-    // Email sending disabled
-};
-
-module.exports = { sendEmail };
