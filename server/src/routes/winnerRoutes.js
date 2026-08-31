d182cfe3ab1587b1cc0a73c638ad723437da88e8
digiflash555 <digiflash555@gmail.com>
2026-08-31 18:23:15 +0530

    Update winnerRoutes.js

--- server/src/routes/winnerRoutes.js
+++ server/src/routes/winnerRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { 
     publishWinners, getWinnersByEvent, createWinner, deleteWinner, getAllWinners,
-    getMyWinnerStatus, uploadWinnerPhoto, deleteWinnerPhoto, lockWinnerPhotos, lockSingleWinnerPhoto
+    getMyWinnerStatus, uploadWinnerPhoto, deleteWinnerPhoto, lockWinnerPhotos, lockSingleWinnerPhoto,
+    likeWinner, commentWinner, likeComment, replyToComment, likeReply
 } = require('../controllers/winnerController');
 const { protect, authorize } = require('../middlewares/authMiddleware');
 const { createCloudinaryUpload } = require('../utils/cloudinaryUpload');
@@ -21,5 +22,11 @@ router.post('/:id/photo', protect, upload.single('photo'), uploadWinnerPhoto);
 router.delete('/:id/photo', protect, authorize('Admin'), deleteWinnerPhoto);
 router.patch('/:id/lock-photo', protect, authorize('Admin'), lockSingleWinnerPhoto);
 router.patch('/event/:eventId/lock-photos', protect, authorize('Admin'), lockWinnerPhotos);
+
+router.post('/:id/like', protect, likeWinner);
+router.post('/:id/comment', protect, commentWinner);
+router.post('/:id/comment/:commentId/like', protect, likeComment);
+router.post('/:id/comment/:commentId/reply', protect, replyToComment);
+router.post('/:id/comment/:commentId/reply/:replyId/like', protect, likeReply);
 
 module.exports = router;
